d28830963a7b47280671626ca5d21f6b40eded03
518H0090 <73675587+518H0090@users.noreply.github.com>
2020-10-30 08:38:19 +0700

    Update HelloWorld.java
    Sửa vậy được chưa

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,12 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-<<<<<<< HEAD
-		Scanner vn = new Scanner(System.in);
-		System.out.println("Hello Bạn Nhé");
-=======
-		System.out.println("haheafwa");
->>>>>>> 8df3a19ecc453319138fb206c4a8bf6660eb28c3
+		System.out.println("Trung Hieu");
 	}
 
 }
