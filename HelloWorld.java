e1c98221753f35a32715d5db0642365fd6a02202
518H0090 <73675587+518H0090@users.noreply.github.com>
2020-10-30 08:36:26 +0700

    Update HelloWorld.java
    fwafwafwa

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,12 @@
 package HelloWorld;
-
+import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Hoi tao di");
+		Scanner vn = new Scanner(System.in);
+		System.out.println("Hello Bạn Nhé");
 	}
 
 }
